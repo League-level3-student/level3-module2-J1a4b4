c40dd51fc1dd28065603998943fd483b12aa8e01
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-06-13 12:59:43 -0700

    SortedArrayChecker, SwappingDigits (IP)
    :smiley:

--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,20 +24,35 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		int placeHolder = arr[1];
+		arr[1] = arr[0];
+		arr[0] = placeHolder;
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
+		while (_00_SortedArrayChecker.intArraySorted(arr) == false) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					int placeHolder = arr[i + 1];
+					arr[i + 1] = arr[i];
+					arr[i] = placeHolder;
+				}
+			}
+		}
 	}
 	
 	//3. Complete the method so that it finds the middle number in the array.
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		sortIntArray(arr);
+		if (arr.length % 2 == 0) {
+			return arr[arr.length / 2];
+		}else {
+			return arr[(arr.length - 1)/ 2];
+		}
 	}
 }
